89e8079bcbdbed77a4ecbc83a3d01a54e3eaafef
Max <maxrivadeneira92@gmail.com>
2025-04-09 14:01:25 +0200

    adding form

--- javascript.js
+++ javascript.js
@@ -1,7 +1,36 @@
 
-document.getElementById("#Acomp").scrollIntoView({behavior: 'smooth'});
-
+document.onload = (event) => {
+    let btnSubmit = document.querySelector("#btn-form-submit")
+    btnSubmit.addEventListener("click", function(e) {
+        console.log("Clack!");
+    }) 
+}
 let btnMenu = document.getElementById("#btn-menu");
+
+
+
+function selectOption() {
+
+
+}
+
+var form = document.querySelector('#form-RSVP'),
+    url = 'https://script.google.com/macros/s/AKfycbwY5CfUC1niDgsQgwXYJ15fKvjR9jYTRG57aRahITVZv1_1szIXkOYTwjqli97WYX0l/exec'
+
+document.querySelector("#btn-form-submit").addEventListener('click', function(e) {
+    console.log("Click!")
+    e.preventDefault();
+    var jqxhr = $.ajax({
+        
+        url: url,
+        method: "GET",
+        dataType: "json",
+        data: $form.serializeObject()
+    }
+    ).success(
+        // do something
+    );
+    })
 
 function showMenu() {
     let navMenu = document.querySelector("#nav-menu");
