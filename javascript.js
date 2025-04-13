d07324165754e31f57d46b512b9ea3d48db41c30
Max <maxrivadeneira92@gmail.com>
2025-04-13 13:57:27 +0200

    nav bar stays in fullscreen and hides in mobile

--- javascript.js
+++ javascript.js
@@ -16,7 +16,7 @@ function selectOption() {
 
 var form = document.querySelector('#form-RSVP'),
     url = 'https://script.google.com/macros/s/AKfycbwY5CfUC1niDgsQgwXYJ15fKvjR9jYTRG57aRahITVZv1_1szIXkOYTwjqli97WYX0l/exec'
-
+/*
 document.querySelector("#btn-form-submit").addEventListener('click', function(e) {
     console.log("Click!")
     e.preventDefault();
@@ -31,14 +31,16 @@ document.querySelector("#btn-form-submit").addEventListener('click', function(e)
         // do something
     );
     })
-
+*/
 function showMenu() {
     let navMenu = document.querySelector("#nav-menu");
     
-    if (navMenu.style.display == "block") {
-        navMenu.style.display = "none";
-    } else {
-        document.querySelector("#nav-menu").style.display = "block";
+    if (screen.width < 800){
+        if (navMenu.style.display == "block") {
+            navMenu.style.display = "none";
+        } else {
+            navMenu.style.display = "block";
+        } 
     }
 }
 
